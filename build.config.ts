7ec4f4a3c41abe74210742b6004ce1c19763754a
hacxy <hacxy.js@outlook.com>
2024-11-27 17:51:40 +0800

    fix: 修复defineConfig方法类型丢失的问题

--- build.config.ts
+++ build.config.ts
@@ -1,9 +1,9 @@
-import { defineBuildConfig } from "unbuild";
+import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
-  entries: ["src/index"],
+  entries: ['src/index'],
   clean: true,
-  declaration: false,
+  declaration: true,
   sourcemap: false,
-  rollup: { esbuild: { target: "node18", minify: true } },
+  rollup: { esbuild: { target: 'node18', minify: true } }
 });
